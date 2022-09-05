9416d146986cea4b899132816f4692b366de94f2
Mattia <xxmattiametryxx@gmail.com>
2022-09-05 14:41:54 +0200

    shapes added

---
+++ content/day01/shapes/script.js
@@ -0,0 +1,65 @@
+//let THREE, scene;
+let triangle, square, tet, box;
+function createScene(THREE, scene) {
+    //THREE = THREE;
+    //scene = scene;
+    let material = customColorsMaterial(THREE, [new THREE.Vector4(1.0,0.158,0.883,0.0), new THREE.Vector4(1.0,0.307,0.307,1.0)]);
+    let materialToon = customToonMaterial(THREE, 0xff02DE, 8);
+
+    // 2D triangle
+    const t_geometry = new THREE.BufferGeometry();
+    const t_vertices = new Float32Array( [
+        -1.0, -1.0,  1.0,
+         1.0, -1.0,  1.0,
+         0,  1.0,  1.0,
+    ] );
+    t_geometry.setAttribute( 'position', new THREE.BufferAttribute( t_vertices, 3 ) );
+    t_geometry.scale(0.2, 0.2, 0.2);
+    triangle = new THREE.Mesh( t_geometry, material );
+    triangle.position.set(-0.5,0.5,0)
+    scene.add( triangle );
+
+    // 2D square
+    const s_geometry = new THREE.BufferGeometry();
+    const s_vertices = new Float32Array( [
+        -1.0, -1.0,  1.0,
+        1.0, -1.0,  1.0,
+        1.0,  1.0,  1.0,
+
+        1.0,  1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        -1.0, -1.0,  1.0
+    ] );
+    s_geometry.setAttribute( 'position', new THREE.BufferAttribute( s_vertices, 3 ) );
+    s_geometry.scale(0.2, 0.2, 0.2);
+    square = new THREE.Mesh( s_geometry, material );
+    square.position.set(0.5,0.5,0)
+    scene.add( square );
+
+    // 3D Tetrahedron
+    const tet_geometry = new THREE.TetrahedronGeometry(0.3);
+    tet = new THREE.Mesh( tet_geometry, material );
+    tet.rotation.z = Math.PI / 4;
+    tet.rotation.x = -Math.PI / 4;
+    tet.position.set(-0.5,-0.5)
+    scene.add( tet );
+
+    // 3D Box
+    const box_geometry = new THREE.BoxGeometry(0.3,0.3,0.3);
+    box = new THREE.Mesh( box_geometry, material );
+    box.rotation.z = Math.PI / 4;
+    box.rotation.x = -Math.PI / 4;
+    box.position.set(0.5,-0.5)
+    scene.add( box );
+}
+
+let i = 0;
+function myAnimation() {
+    tet.rotation.x = i;
+    tet.rotation.y = i;
+
+    box.rotation.x = i;
+    box.rotation.y = i;
+
+    i+= 0.01;
+}
--- readme.md
+++ readme.md
@@ -4,6 +4,6 @@
 To start out, we made a few Conditional Designs with pen and paper. This helped me to get my mind back into understanding and thinking about how random things can turn out when certain conditions are given.
 
 ## Computing with computer
-Three JS setup test
+To get back into coding visuals, I started to draw some simple shapes with Three.js
 {% raw %}
-<iframe src="template_three/index.html" width="100%" height="450" frameborder="no"></iframe> {% endraw %}
+<iframe src="content/day01/shapes/index.html" width="100%" height="450" frameborder="no"></iframe> {% endraw %}
